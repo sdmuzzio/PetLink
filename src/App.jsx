3a844e8987c1b836359ef281c70b3a717749b6af
sdmuzzio <sdmuzzio@gmail.com>
2025-11-03 14:18:50 -0300

    cambio de nombre de carpeta

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { LandingPage } from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Inicio from './pages/Inicio';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/inicio" element={<Inicio />} />
       </Routes>
     </div>
   );
