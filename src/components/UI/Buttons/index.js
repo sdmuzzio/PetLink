be21c7c386528afc7bbbebee2f01a20965825de4
sdmuzzio <sdmuzzio@gmail.com>
2025-11-05 18:42:09 -0300

    Update button exports and landing page styles
    Refactored button exports in index.js to use named exports for all buttons, including BtnBack, BtnHome, and BtnOfertas. Adjusted landing page CSS dimensions for height and width to improve layout consistency.

--- src/components/UI/Buttons/index.js
+++ src/components/UI/Buttons/index.js
@@ -1,6 +1,11 @@
-// src/components/UI/Buttons/index.js
-export { BtnPrimary } from './BtnPrimary';
-export { BtnSecondary } from './BtnSecondary';
-export { BtnDanger } from './BtnDanger';
-export { BtnLink } from './BtnLink';
-export { BtnPropuestas } from './BtnPropuestas';
+// Esta línea significa:
+// "Toma el 'export default' del archivo './BtnBack' 
+//  y re-expórtalo como un 'export nombrado' llamado 'BtnBack'"
+export { default as BtnBack } from './BtnBack';
+export { default as BtnDanger } from './BtnDanger';
+export { default as BtnHome } from './BtnHome';
+export { default as BtnLink } from './BtnLink';
+export { default as BtnOfertas } from './BtnOfertas';
+export { default as BtnPrimary } from './BtnPrimary';
+export { default as BtnPropuestas } from './BtnPropuestas';
+export { default as BtnSecondary } from './BtnSecondary';
